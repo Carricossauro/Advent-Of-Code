53fd831d585a2526f2f2d03b9ba10e585f11be59
Tiago Carriço <tiagocarrico27@gmail.com>
2021-12-21 14:38:46 +0000

    day14 part2

--- 2021/day14/day14_part2.py
+++ 2021/day14/day14_part2.py
@@ -2,34 +2,56 @@
 
 import collections
 
-def step(rules, polymer):
-	pairs = []
-	for i in range(len(polymer)-1):
-		pairs.append("".join([polymer[i], polymer[i+1]]))
-
-	newPolymer = []
-	for i, pair in enumerate(pairs):
-		if i == 0:
-			newPolymer.append(pair[0])
-
+def step(rules, frequencies, pairs):
+	pairList = list(pairs.items())
+	for pair, freq in pairList:
 		if pair in rules:
-			newPolymer.append(rules[pair])
-			newPolymer.append(pair[1])
-		elif i == len(polymer) - 1:
-			newPolymer.append(pair[1])
-	return "".join(newPolymer)
+			x, y = pair
+			newElem = rules[pair]
+			pairs[pair] -= freq
+			if (x, newElem) not in pairs:
+				pairs[(x, newElem)] = 0
+			if (newElem, y) not in pairs:
+				pairs[(newElem, y)] = 0
+			pairs[(x, newElem)] += freq
+			pairs[(newElem, y)] += freq
+			if newElem not in frequencies:
+				frequencies[newElem] = 0
+			frequencies[newElem] += freq
+
+	return frequencies, pairs
 
 file = open("day14.txt", 'r')
 
 lines = file.readlines()
 
 polymer = lines[0].strip()
-rules = dict(map(lambda x: tuple(x.strip().split(" -> ")),lines[2:]))
+rules = dict([((k[0], k[1]), v) for k, v in map(lambda x: tuple(x.strip().split(" -> ")),lines[2:])])
 
-for _ in range(40):
-	polymer = step(rules, polymer)
+frequencies = {}
+pairs = {}
+
+for i, elem in enumerate(polymer):
+	if elem not in frequencies:
+		frequencies[elem] = 0
+	frequencies[elem] += 1
+	if i > 0:
+		pair = (polymer[i-1], elem)
+		if pair not in pairs:
+			pairs[pair] = 0
+		pairs[pair] += 1
 
 print(polymer)
+print(frequencies)
+print(pairs)
+print(rules)
 
-frequencies = dict(collections.Counter(polymer)).values()
-print(max(frequencies) - min(frequencies))
+for _ in range(40):
+	frequencies, pairs = step(rules, frequencies, pairs)
+
+print("frequencies", frequencies)
+print("pairs", pairs)
+print("rules", rules)
+
+frequencyList = frequencies.values()
+print(max(frequencyList) - min(frequencyList))
