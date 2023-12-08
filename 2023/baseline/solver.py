02c064bab0ea64d3b9924f4250a0c4066d369e08
Carricossauro <tiagocarrico01@gmail.com>
2023-12-08 12:12:23 +0100

    day 0 - added baseline (refactor)

--- 2023/baseline/solver.py
+++ 2023/baseline/solver.py
@@ -17,10 +17,20 @@ class Solver:
         return None
     
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and 1 <= int(sys.argv[1]) <= 2:
-        if sys.argv[1] == '1':
+    if len(sys.argv) > 2 and 1 <= int(sys.argv[1]) <= 2:
+
+        if sys.argv[2] == 'test':
+            solver = Solver("test.txt")
+        elif sys.argv[2] == 'test2':
+            solver = Solver("test2.txt")
+        elif sys.argv[2] == '1':
             solver = Solver("input1.txt")
+        elif sys.argv[2] == '2':
+            solver = Solver("input2.txt")
+
+        if sys.argv[1] == '1':
             print(solver.part1())
         else:
-            solver = Solver("input1.txt")
             print(solver.part2())
+    else:
+        print("Usage: python3 solver.py [1 / 2] [test / test2 / 1 / 2]")
